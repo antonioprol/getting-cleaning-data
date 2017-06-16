45874df5c1db4bb3b6db9d6a3f1360a2455ea670
Miguel Prol <mprol@com.uvigo.es>
2017-06-16 10:20:57 +0200

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# getting-cleaning-data
+## Course Project: Getting and Cleaning Data
 This is the repository for the 'Getting and Cleaning Data' Course Project
+Student: antonioprol
+
+## 'run_analysis.R'. Steps:
+
+### 1. Reading the necessary files and loading into objects
+
+Adding to "testset" ("X_test.txt") and "trainset" ("X_train.txt") descriptive names for the features columns.
+
+### 2.1 Adding the subject id and the activity id to the 'test' and the 'train' datasets
+
+a. cbind() columns "subject_test" ("subject_test.txt") and "test_labels" ("y_test.txt") to "testset" ("X_test.txt")
+b. cbind() columns "subject_train" ("subject_train.txt") and "train_labels" ("y_train.txt") to "trainset" ("X_train.txt")
+
+At first "testset" and "trainset" have dimensions 2,947 × 561 and 7,352 × 561, respectively.
+Plus 2 columns would be --> 2,947 × 563 and 7,352 × 563, respectively.
+
+### 2.2 ... and clipping the 'test' and 'train' datasets together + adding descriptive activity names
+
+a. rbind() 'test' and 'train' datasets
+b. merge() with activity names and select out unwanted id column
+
+At this point we get a data frame (data) with dimensions: 10,299 × 563
+
+### 3.1 Selecting all features with 'mean' or 'std' in their names, with either upper or lower case
+
+dim(data_s):  10,299 × 88
+
+### 3.2 ... and melting all the 'features' columns to obtain a tidy data set (dataMelt)
+
+dim(dataMelt): 885,714 × 4
+
+### 4. Obtaining the final tidy dataset (dataMelt_g) with the average of each variable for each activity and each subject
+
+dim(dataMelt_g): 15,480 x 4
---
+++ run_analysis.R
@@ -0,0 +1,30 @@
+setwd("./getting-cleaning-data")  # wd for the 'Getting and Cleaning Data' Course Project
+library(dplyr)
+library(reshape2)
+dpathbase <- "UCI HAR Dataset/"
+dpathtest <- paste0(dpathbase, "test/")
+dpathtrain <- paste0(dpathbase, "train/")
+
+# Reading the necessary files and loading into objects
+activities <- tbl_df(read.table(paste0(dpathbase, "activity_labels.txt"), col.names= c("id", "activity")))
+features <- tbl_df(read.table(paste0(dpathbase, "features.txt"), col.names= c("ind", "names")))
+test_labels <- tbl_df(read.table(paste0(dpathtest, "y_test.txt"), col.names= "act_id"))
+train_labels <- tbl_df(read.table(paste0(dpathtrain, "y_train.txt"), col.names= "act_id"))
+subject_test <- tbl_df(read.table(paste0(dpathtest, "subject_test.txt"), col.names= "subject_id"))
+subject_train <- tbl_df(read.table(paste0(dpathtrain, "subject_train.txt"), col.names= "subject_id"))
+testset <- tbl_df(read.table(paste0(dpathtest, "X_test.txt"), col.names=features$names, colClasses = "numeric"))
+trainset <- tbl_df(read.table(paste0(dpathtrain, "X_train.txt"), col.names=features$names, colClasses = "numeric"))
+
+# Clipping the 'test' and 'train' datasets together
+data0 <- tbl_df(rbind(cbind(subject_test, test_labels, testset), cbind(subject_train, train_labels, trainset)))
+data <- tbl_df(merge(activities, data0, by.x="id", by.y="act_id"))  %>% select(-id)
+
+# Selecting all features with 'mean' or 'std' in their names, with either upper or lower case
+data_s <- select(data, activity, subject_id, matches("mean|std"))  # 'matches(x, ignore.case = TRUE)' by default
+
+# 'dataMelt' is tidy
+mvars <- names(data_s)[!(names(data_s) %in% c("activity", "subject_id"))]  # all the column names except 'activity' and 'subject_id'
+dataMelt <- tbl_df(melt(data_s, id=c("activity", "subject_id"), measure.vars=mvars))
+
+# Final tidy dataset
+dataMelt_g <- dataMelt %>% group_by(activity, subject_id, variable) %>% summarize(mean=mean(value))
